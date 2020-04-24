f639f1b5038e7e8914966fc34abe07156452bd9c
cara <xw33@outlook.com>
2020-04-24 19:50:23 +0100

    change database structure; add authors table

--- Frontend/src/helpers/jsonToCSV.js
+++ Frontend/src/helpers/jsonToCSV.js
@@ -2,26 +2,41 @@ const fs = require("fs");
 const path = require("path");
 // const certPath = path.join(__dirname, "./poems.json");
 // const poems = JSON.parse(fs.readFileSync(certPath));
-const poems = require('./poems.json')
+const poems = require("./poems.json");
 
-let csvContent = "";
+let allPoems = "";
+let allAuthors = "";
 const poemCollection = [];
+const authorList = [];
+
 poems.forEach((poemObj) => {
   const poemArr = [];
-  poemArr.push(poems.indexOf(poemObj)+1);
+  poemArr.push(poems.indexOf(poemObj) + 1);
   poemArr.push(poemObj.author);
   poemArr.push(poemObj.title);
   poemArr.push(poemObj.lines);
   poemCollection.push(poemArr);
+  authorList.push(poemObj.author);
 });
+
+authors = new Set(authorList);
+authorCollection = [...authors];
 
 poemCollection.forEach((poemArr) => {
   let row = poemArr.join("\t");
-  csvContent += row + "\r\n";
+  allPoems += row + "\r\n";
 });
 
-fs.writeFile('./poems.csv', csvContent, function(err) {
-  if(err) {}})
+authorCollection.forEach((author, index) => {
+  let row = index + 1 + "\t" + author;
+  allAuthors += row + "\r\n";
+});
 
-
-
+fs.writeFile("./poems.csv", allPoems, function (err) {
+  if (err) {
+  }
+});
+fs.writeFile("./authors.csv", allAuthors, function (err) {
+  if (err) {
+  }
+});
