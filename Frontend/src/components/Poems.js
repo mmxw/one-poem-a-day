319a9a2f152a6d7ef78d1fb8caba6917f05eaa29
cara <xw33@outlook.com>
2020-04-25 23:35:28 +0100

    display all poem titles by alphabetical order

--- Frontend/src/components/Poems.js
+++ Frontend/src/components/Poems.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { ListGroup } from "react-bootstrap";
+const poems = require('../helpers/poems.json');
 
 export default function Poems() {
   
+  const titles = poems.map(poem => poem.title)
+  const sortedTitles = titles.sort()
+  
   return (
     <div>
-      
+      <ListGroup>
+        {sortedTitles.map((title, index) => (
+          <ListGroup.Item action href="#" key={index}>
+            {title}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   )
 }
